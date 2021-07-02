69ca1d80603c2bea67e97f477aad9e5099391f7a
AW <codingdayandnight@gmail.com>
2021-07-02 18:42:16 +0200

    let runners run added

--- source/bot/Bot.js
+++ source/bot/Bot.js
@@ -13,8 +13,12 @@ export default class Bot {
   highestPriceRecorded = -Infinity;
   buyCountTotal = 0;
   sellCountTotal = 0;
+  buyResetCount = 0;
+  sellResetCount = 0;
   count = 0;
+  onLastPriceHasRunAtLeastOnce = false;
   tradeHistory = []; // not added to store atm
+  letRunnersRun = process.argv.includes("lrr");
 
   constructor(data) {
     this.data = data;
@@ -44,7 +48,8 @@ export default class Bot {
 
   getConfigAndResultsAndTradeHistory() {
     return {
-      tradeHistory: this.tradeHistory,
+      hands: this.hands,
+      // tradeHistory: this.tradeHistory,
       config: this.data.config,
       results: store.getResults(this.itsAccountId, this.id),
     };
@@ -73,6 +78,8 @@ export default class Bot {
       quoteTotalIncludingBaseSoldAsPlanned,
       buyCountTotal: this.buyCountTotal,
       sellCountTotal: this.sellCountTotal,
+      buyResetCount: this.buyResetCount,
+      sellResetCount: this.sellResetCount,
       lastPrice: this.lastPrice,
       lowestPriceRecorded: this.lowestPriceRecorded,
       highestPriceRecorded: this.highestPriceRecorded,
@@ -82,7 +89,7 @@ export default class Bot {
   onLastPrice(pairs) {
     const pair = pairs[this.data.config.pair];
 
-    if (!pair) return;
+    if (!pair || !pair.close) return;
 
     const lastPrice = parseFloat(pair.close);
 
@@ -98,6 +105,24 @@ export default class Bot {
       // console.log(lastPrice);
     }
 
+    if (this.letRunnersRun) {
+      this.processLastPriceLettingRunnersRun(lastPrice);
+    } else {
+      this.processLastPriceStandard(lastPrice);
+    }
+  }
+
+  quoteCurrencyIsAvailable(hand, minTransactionValueRequiredByExchange) {
+    // return hand.quote >= minTransactionValueRequiredByExchange;
+    return hand.quote > 0;
+  }
+
+  baseCurrencyIsAvailable(hand, minTransactionValueRequiredByExchange) {
+    // return hand.base * this.lastPrice >= minTransactionValueRequiredByExchange;
+    return hand.base > 0;
+  }
+
+  processLastPriceStandard(lastPrice) {
     const buyingHands = this.hands.filter(
       (hand) => !hand.bought && lastPrice < hand.buyBelow
     );
@@ -112,6 +137,118 @@ export default class Bot {
 
     sellingHands.forEach((hand) => {
       this.sell(hand, lastPrice);
+    });
+  }
+
+  processLastPriceLettingRunnersRun(lastPrice) {
+    const minTransactionValueRequiredByExchange = 0.000001; // todo: actually it is > 10 USDT
+
+    if (!this.onLastPriceHasRunAtLeastOnce) {
+      this.hands.forEach((hand) => {
+        if (
+          this.quoteCurrencyIsAvailable(
+            hand,
+            minTransactionValueRequiredByExchange
+          ) &&
+          lastPrice < hand.buyBelow
+        ) {
+          // console.log("first run buy by hand", hand.id, "at", lastPrice);
+          this.buy(hand, lastPrice);
+        }
+      });
+
+      this.onLastPriceHasRunAtLeastOnce = true;
+
+      return;
+    }
+
+    const stepPercent = 0.001; // temp
+
+    this.hands
+      .filter((hand) => hand.readyToBuy)
+      .forEach((hand) => {
+        if (
+          !this.quoteCurrencyIsAvailable(
+            hand,
+            minTransactionValueRequiredByExchange
+          )
+        ) {
+          // hand has no quote - do nothing
+        } else if (lastPrice > hand.stopBuy && lastPrice < hand.sellAbove) {
+          this.buy(hand, lastPrice);
+          // console.log(lastPrice, "stop buy triggered on hand", hand.id);
+          // hand.bought = true; // if bought successfully
+          hand.readyToBuy = false; // if bought successfully
+          hand.stopBuy = hand.buyBelow;
+        } else if (hand.stopBuy - lastPrice > hand.stopBuy * stepPercent) {
+          hand.stopBuy -= (hand.stopBuy - lastPrice) / 2;
+          this.buyResetCount++;
+          // console.log(
+          //   lastPrice,
+          //   "stop buy on hand",
+          //   hand.id,
+          //   "reset to",
+          //   hand.stopBuy
+          // );
+        }
+      });
+
+    this.hands.forEach((hand) => {
+      if (
+        !this.quoteCurrencyIsAvailable(
+          hand,
+          minTransactionValueRequiredByExchange
+        )
+      ) {
+        // hand has no quote - do nothing
+      } else if (!hand.readyToBuy && lastPrice < hand.buyBelow) {
+        // console.log("marking hand", hand.id, "ready to buy, at", lastPrice);
+        hand.readyToBuy = true;
+      }
+    });
+
+    this.hands
+      .filter((hand) => hand.readyToSell)
+      .forEach((hand) => {
+        if (
+          !this.baseCurrencyIsAvailable(
+            hand,
+            minTransactionValueRequiredByExchange
+          )
+        ) {
+          // hand has no base - do nothing
+        } else if (lastPrice < hand.stopSell && lastPrice > hand.buyBelow) {
+          this.sell(hand, lastPrice);
+          // console.log("stop sell triggered on hand", hand.id, "at", lastPrice);
+          // hand.bought = false; // if sold successfully
+          hand.readyToSell = false; // if sold successfully
+          hand.stopSell = hand.sellAbove;
+        } else if (lastPrice - hand.stopSell > hand.stopSell * stepPercent) {
+          hand.stopSell += (lastPrice - hand.stopSell) / 2;
+          this.sellResetCount++;
+          // console.log(
+          //   "stop sell on hand",
+          //   hand.id,
+          //   "reset to",
+          //   hand.stopSell,
+          //   "at",
+          //   lastPrice
+          // );
+        }
+      });
+
+    this.hands.forEach((hand) => {
+      if (
+        !this.baseCurrencyIsAvailable(
+          hand,
+          minTransactionValueRequiredByExchange
+        )
+      ) {
+        // hand has no base - do nothing
+      } else if (!hand.readyToSell && lastPrice > hand.sellAbove) {
+        hand.readyToSell = true;
+        // console.log("marking hand", hand.id, "ready to sell, at", lastPrice);
+      }
     });
   }
 
