92de6bd990b469d5be75cbe97bb6174b3acab280
AW <codingdayandnight@gmail.com>
2022-01-04 16:37:03 +0100

    example historical csv file added

--- source/bot/botConfig.ts
+++ source/bot/botConfig.ts
@@ -6,10 +6,10 @@ import { BotConfig } from "../types";
 const botConfigs: BotConfig[] = [
   {
     symbol: "BTC-USDT",
-    from: 29000,
-    to: 36000,
-    quoteFrom: 29000,
-    quoteTo: 36000,
+    from: 31000,
+    to: 59000,
+    quoteFrom: 31000,
+    quoteTo: 59000,
     baseFrom: 0,
     baseTo: 0,
     handCount: null,
@@ -26,28 +26,6 @@ const botConfigs: BotConfig[] = [
     id: null,
     itsAccountId: null,
   },
-  // {
-  //   symbol: "BTC-USDT",
-  //   from: 49000,
-  //   to: 52000,
-  //   quoteFrom: 49000,
-  //   quoteTo: 52000,
-  //   baseFrom: 49000,
-  //   baseTo: 52000,
-  //   handCount: null,
-  //   handSpan: 0.006,
-  //   quoteStartAmount: 8,
-  //   quoteStartAmountPerHand: null,
-  //   baseStartAmount: 0,
-  //   baseStartAmountPerHand: null,
-  //   exchangeFee: 0.001,
-  //   baseMinimumTradeSize: null,
-  //   quoteMinimumTradeSize: null,
-  //   baseIncrement: null,
-  //   quoteIncrement: null,
-  //   id: null,
-  //   itsAccountId: null,
-  // },
 ];
 
 export default botConfigs;
