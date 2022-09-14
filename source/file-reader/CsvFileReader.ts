e35204f46193107eb045a1662b06ec300d50933b
AW <codingdayandnight@gmail.com>
2022-09-14 14:11:24 +0200

    last blank line in csv file fixed

--- source/file-reader/CsvFileReader.ts
+++ source/file-reader/CsvFileReader.ts
@@ -1,20 +1,23 @@
-import fs from "fs";
+import fs from 'fs'
 
 export default class CsvFileReader {
   static getAsString(filePath): string {
-    const file: Buffer = fs.readFileSync(filePath);
-    return file.toString();
+    const file: Buffer = fs.readFileSync(filePath)
+    return file.toString()
   }
 
   static getRowsHoldingStrings(filePath): string[] {
-    return CsvFileReader.getAsString(filePath).split(/\r?\n/);
+    return CsvFileReader.getAsString(filePath).split(/\r?\n/)
   }
 
   static getRowsPopulatedWithNumbers(filePath): number[][] {
-    return CsvFileReader.getRowsHoldingStrings(
-      filePath
-    ).map((rowAsString: string) =>
-      rowAsString.split(",").map((value: string) => parseFloat(value))
-    );
+    return CsvFileReader.getRowsHoldingStrings(filePath)
+      .map((rowAsString: string) =>
+        rowAsString.split(',').map((value: string) => parseFloat(value))
+      )
+      .filter(
+        (rowWithNumber: number[]) =>
+          !isNaN(rowWithNumber[0]) && typeof rowWithNumber[0] === 'number'
+      )
   }
 }
--- source/price-reader/PriceReader.ts
+++ source/price-reader/PriceReader.ts
@@ -3,25 +3,25 @@
  * (kucoin ticker fires at 100ms interval)
  */
 
-import "dotenv/config";
-import eventBus from "../events/eventBus.js";
-import CsvFileReader from "../file-reader/CsvFileReader.js";
+import 'dotenv/config'
+import eventBus from '../events/eventBus.js'
+import CsvFileReader from '../file-reader/CsvFileReader.js'
 import {
   KucoinNodeApiTickerMessage,
   PriceStreamCallbackParameters,
-} from "../types";
-import Messages from "../types/messages.js";
-import { Exchange } from "../exchange/Exchange.js";
+} from '../types'
+import Messages from '../types/messages.js'
+import { Exchange } from '../exchange/Exchange.js'
 
 export default class PriceReader {
-  static cachedFileContent: { [key: string]: number[][] } = {};
-  static maxPossiblePrice: number = 2000000;
-  static dateMs: number = Date.now();
-  static callbackIntervalDefaultMs: number = 1000;
+  static cachedFileContent: { [key: string]: number[][] } = {}
+  static maxPossiblePrice: number = 2000000
+  static dateMs: number = Date.now()
+  static callbackIntervalDefaultMs: number = 1000
   static callbackIntervalMs: number = parseInt(
     process.env.LAST_PRICE_CALLBACK_INTERVAL_MS ||
       String(PriceReader.callbackIntervalDefaultMs)
-  );
+  )
 
   static startOneSymbolLivePriceStream(
     symbol: string,
@@ -29,24 +29,24 @@ export default class PriceReader {
   ) {
     Exchange.startWSTicker(symbol, (tickerMessageAsString: string) => {
       const intervalNotCompleted: boolean =
-        Date.now() < PriceReader.dateMs + PriceReader.callbackIntervalMs;
+        Date.now() < PriceReader.dateMs + PriceReader.callbackIntervalMs
 
-      if (intervalNotCompleted) return;
+      if (intervalNotCompleted) return
 
-      PriceReader.dateMs = Date.now();
+      PriceReader.dateMs = Date.now()
 
       const message: KucoinNodeApiTickerMessage = JSON.parse(
         tickerMessageAsString
-      );
+      )
 
-      if (!message.data?.price) return;
+      if (!message.data?.price) return
 
-      const lastPrice: number = parseFloat(message.data.price);
+      const lastPrice: number = parseFloat(message.data.price)
 
       if (PriceReader.priceIsValid(lastPrice)) {
-        callback(lastPrice);
+        callback(lastPrice)
       }
-    });
+    })
   }
 
   static startAllSymbolsLivePriceStream(
@@ -56,53 +56,51 @@ export default class PriceReader {
       /* this callback runs once per each symbol message received */
       const message: KucoinNodeApiTickerMessage = JSON.parse(
         tickerMessageAsString
-      );
+      )
 
-      if (!message.data?.price) return;
+      if (!message.data?.price) return
 
-      const symbol: string = message.subject;
-      const lastPrice: number = parseFloat(message.data.price);
+      const symbol: string = message.subject
+      const lastPrice: number = parseFloat(message.data.price)
 
       if (PriceReader.priceIsValid(lastPrice)) {
-        callback({ symbol, lastPrice });
+        callback({ symbol, lastPrice })
       }
-    });
+    })
   }
 
   static startHistoricalStream(filePaths: string[], column: number) {
     filePaths.forEach((filePath: string) => {
       const rowsPopulatedWithNumbers: number[][] =
         this.cachedFileContent[filePath] ||
-        CsvFileReader.getRowsPopulatedWithNumbers(filePath);
+        CsvFileReader.getRowsPopulatedWithNumbers(filePath)
 
-      this.cachedFileContent[filePath] = rowsPopulatedWithNumbers;
+      this.cachedFileContent[filePath] = rowsPopulatedWithNumbers
 
       rowsPopulatedWithNumbers.forEach((row: number[], i: number) => {
-        const price: number = row[column];
-        const isLastBlankLine: boolean =
-          i === rowsPopulatedWithNumbers.length - 1 && price === undefined;
+        const price: number = row[column]
 
-        if (!isLastBlankLine && PriceReader.priceIsValid(price)) {
+        if (PriceReader.priceIsValid(price)) {
           eventBus.emit(eventBus.events!.LAST_PRICE, {
             symbol: process.env.SYMBOL_GLOBAL,
             lastPrice: price,
-          });
+          })
         }
-      });
-    });
+      })
+    })
 
-    eventBus.emit(eventBus.events!.HISTORICAL_PRICE_READER_FINISHED);
+    eventBus.emit(eventBus.events!.HISTORICAL_PRICE_READER_FINISHED)
   }
 
   private static priceIsValid(price: number): boolean {
     if (isNaN(price)) {
-      console.log(`${price} ${Messages.IS_NOT_A_NUMBER}`);
-      return false;
+      console.log(`${price} ${Messages.IS_NOT_A_NUMBER}`)
+      return false
     }
 
     const priceOutsideBounds: boolean =
-      price <= 0 || price > PriceReader.maxPossiblePrice;
+      price <= 0 || price > PriceReader.maxPossiblePrice
 
-    return !priceOutsideBounds;
+    return !priceOutsideBounds
   }
 }
