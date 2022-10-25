df26779ee271bd44f72baba4a96146c7f12e5d6f
AW <codingdayandnight@gmail.com>
2022-10-25 08:47:10 +0200

    const added

--- source/account/account.ts
+++ source/account/account.ts
@@ -1,17 +1,18 @@
-import Bot from "../bot/bot.js";
-import { AccountData, BotData } from "../types";
-import Messages from "../types/messages.js";
+import Bot from '../bot/bot.js'
+import { AccountData, BotData } from '../types'
+import Messages from '../types/messages.js'
 
 export default class Account {
-  bots: Bot[] = [];
+  bots: Bot[] = []
+  minBotCount: number = 1
 
   constructor(data: AccountData) {
-    if (!data.bots || data.bots.length < 1) {
-      throw new Error(Messages.NO_BOT_DATA_AVAILABLE);
+    if (!data.bots || data.bots.length < this.minBotCount) {
+      throw new Error(Messages.NO_BOT_DATA_AVAILABLE)
     }
 
     data.bots.forEach((botData: BotData) => {
-      this.bots.push(new Bot(botData));
-    });
+      this.bots.push(new Bot(botData))
+    })
   }
 }
