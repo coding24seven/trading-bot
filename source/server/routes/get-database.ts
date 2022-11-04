8c6bfa77064100a45be8f11983c22857d539046d
AW <codingdayandnight@gmail.com>
2022-11-04 09:35:32 +0100

    get request query-based guard added

--- source/server/routes/get-database.ts
+++ source/server/routes/get-database.ts
@@ -7,6 +7,12 @@ const router: Router = express.Router()
 export default router
 
 router.get('/', async (req: Request, res: Response) => {
+  if (req.query.x !== 'x') {
+    res.send('Maintenance in progress. Check later.')
+
+    return
+  }
+
   const readResponse: AxiosResponse | string = await store.readDatabase()
 
   res.send(typeof readResponse === 'string' ? readResponse : readResponse.data)
