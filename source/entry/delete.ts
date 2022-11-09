c56ed1945a5e8aedbb4a13e926b90ea6b428addb
AW <codingdayandnight@gmail.com>
2022-11-09 13:22:11 +0100

    delete.ts fixed

--- source/entry/delete.ts
+++ source/entry/delete.ts
@@ -1,17 +1,36 @@
 /*
- * deletes this app's database file
+ * delete this app's database file
  */
 
 import { AxiosResponse } from 'axios'
 import startDBServer from 'trading-bot-database'
-import { setDotEnv } from '../../config/env.js'
+import { setDotEnv, validateEnvVariables } from '../../config/env.js'
 import store from '../store/store.js'
 
-setDotEnv()
+const {
+  HOST_NAME,
+  DATABASE_PORT,
+  DATABASE_DIRECTORY,
+  DATABASE_BACKUP_DIRECTORY,
+}: NodeJS.ProcessEnv = setDotEnv()
+
+const variables = [
+  'HOST_NAME',
+  'DATABASE_PORT',
+  'DATABASE_DIRECTORY',
+  'DATABASE_BACKUP_DIRECTORY',
+]
+
+validateEnvVariables(variables)
 
 void (async function () {
   try {
-    await startDBServer()
+    await startDBServer(
+      parseInt(DATABASE_PORT!),
+      HOST_NAME!,
+      DATABASE_DIRECTORY!,
+      DATABASE_BACKUP_DIRECTORY!
+    )
 
     const response: AxiosResponse | undefined | void =
       await store.deleteDatabase()
@@ -21,6 +40,7 @@ void (async function () {
     }
   } catch (error: any) {
     console.error(error.data)
-    process.exit()
   }
+
+  process.exit()
 })()
